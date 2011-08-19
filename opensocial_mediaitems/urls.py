50799d3615f188fe0c82b55a6e277cd805e8d549
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-19 14:25:11 +0200

    gitignore file added

---
+++ opensocial_mediaitems/urls.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import url
+
+urlpatterns = patterns('opensocial_mediaitems.views',
+                       url(r'^people$',
+                           'people',
+                           {'user': '@me',
+                            'group': '@friends'},
+                            name="people"),            
+                        
+                        (r'^people/(?P<user>@?\w+)$',
+                        'people',
+                        {'group': '@friends'}),
+            
+                        (r'^people/(?P<user>@?\w+)/(?P<group>@?\w+)$',
+                        'people'),
+            
+                        (r'^people/(?P<user>@?\w+)/(?P<group>@?\w+)/(?P<tuser>@?\w+)$',
+                        'people'),
+            
+        )
