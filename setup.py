5bb727d2887bac07688677d642861e0250793291
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-25 12:04:09 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='opensocial_mediaitems',
+    version='1.0.0',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_opensocial_mediaitems',
+    packages=['opensocial_mediaitems'],
+)
